2931d953528bf3da7cd2628991fda19c4c1573a7
TheMrBlackLord <theblacklord05@gmail.com>
2022-02-24 19:26:07 +0300

    create route

--- client/router/router.js
+++ client/router/router.js
@@ -6,7 +6,7 @@ const router = createRouter({
       {
          path: '/',
          name: 'home',
-         // component: () => import('./components/')
+         component: () => import('../src/components/minesweeper/Minesweeper.vue')
       }
    ]
 })
