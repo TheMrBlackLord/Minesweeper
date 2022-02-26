2732d441c1eb83309f64ecaff460d061eb6b182b
TheMrBlackLord <theblacklord05@gmail.com>
2022-02-26 11:11:06 +0300

    add difficulties to store.js

--- client/store/store.js
+++ client/store/store.js
@@ -1,8 +1,18 @@
 import { createStore } from 'vuex'
 
 const store = createStore({
-   state: {},
-   getters: {},
+   state: {
+      difficulties: {
+        easy: {size: 10, bombs: 20, cellSize: 40, fontSize: '25px'},
+        medium: {size: 15, bombs: 45, cellSize: 30, fontSize: '22px'},
+        hard: {size: 20, bombs: 80, cellSize: 25, fontSize: '18px'},
+      }
+   },
+   getters: {
+      getDifficulty(state) {
+         return state.difficulties
+      }
+   },
    mutations: {},
    actions: {}
 })
