ac1efd3003dbe2af762e113901b5d31fdaa97bbc
TheMrBlackLord <theblacklord05@gmail.com>
2022-02-26 11:07:40 +0300

    add vuex store

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from '../router/router'
+import store from '../store/store'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import './styles/style.scss'
 
 const app = createApp(App)
 app.use(router)
+app.use(store)
 app.mount('#app')
