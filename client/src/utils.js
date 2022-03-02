545e8e9ce99c2bad33b9901145585e41260969f3
TheMrBlackLord <theblacklord05@gmail.com>
2022-03-02 14:55:53 +0300

    add functions to utils.js to create game board

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,7 +1,38 @@
+import { chunk, fill } from 'lodash';
 module.exports = {
    getMatrixPosFromIndex(index, size) {
       const row = Math.floor(index / size)
       const col = index % size
       return {row, col}
-    }
+   },
+   getFilledMatrix(size, filler) {
+      return chunk(fill(Array(size * size), filler), size)
+   },
+   calculateBombs(grid) {
+      const size = grid[0].length
+      for (let i = 0; i < size; i++) {
+         for (let j = 0; j < size; j++) {
+            let count = 0
+            if (grid[i][j] === 'x') continue
+            if (i > 0) {
+               if (grid[i - 1][j - 1] === 'x') count++
+               if (grid[i - 1][j] === 'x') count++
+               if (grid[i - 1][j + 1] === 'x') count++
+            }
+            
+            if (j > 0)
+               if (grid[i][j - 1] === 'x') count++
+            if (j < size - 1)
+               if (grid[i][j + 1] === 'x') count++
+
+            if (i < size - 1) {
+               if (grid[i + 1][j - 1] === 'x') count++
+               if (grid[i + 1][j] === 'x') count++
+               if (grid[i + 1][j + 1] === 'x') count++
+            }
+            grid[i][j] = count
+         }
+      }
+      return grid
+   },
 }
