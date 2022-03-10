a43a5db86da3b6f3bed9bc4fc2869f7c166bd989
TheMrBlackLord <theblacklord05@gmail.com>
2022-03-10 18:33:18 +0300

    add cookie parser

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose')
 const authRouter = require('./routes/auth.routes')
 
 const app = express()
 app.use(cors())
+app.use(cookieParser())
 app.use(express.json())
 app.use('/api/auth', authRouter)
 const PORT = process.env.PORT || 5000
